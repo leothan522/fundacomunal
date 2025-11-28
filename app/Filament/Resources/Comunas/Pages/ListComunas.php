26c299154955ade2d1d123ce92c595dd35359767
Yonathan Castillo <leothan522@gmail.com>
2025-11-28 18:03:57 -0400

    Import Excel Comunas

--- app/Filament/Resources/Comunas/Pages/ListComunas.php
+++ app/Filament/Resources/Comunas/Pages/ListComunas.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\Comunas\Pages;
 
 use App\Filament\Resources\Comunas\ComunaResource;
+use App\Imports\ComunaImport;
+use App\Models\Comuna;
+use EightyNine\ExcelImport\ExcelImportAction;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -13,6 +16,9 @@ class ListComunas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            ExcelImportAction::make()
+                ->use(ComunaImport::class)
+                ->hidden(fn(): bool => Comuna::exists()),
             CreateAction::make(),
         ];
     }
---
+++ app/Imports/ComunaImport.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Comuna;
+use App\Models\Estado;
+use App\Models\Municipio;
+use App\Models\Redi;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithStartRow;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+
+class ComunaImport implements ToCollection, WithStartRow
+{
+    /**
+    * @param Collection $collection
+    */
+    public function collection(Collection $collection): void
+    {
+        foreach ($collection as $row){
+            $municipio = $row[0];
+            $parroquia = $row[1];
+            $codCOM = $row[2];
+            $codSITUR = $row[3];
+            $nombre = $row[4];
+            $cantidadCC = intval($row[5]);
+
+            if (is_numeric($codSITUR)){
+                $fecha = Date::excelToDateTimeObject($codSITUR)->format('d-m-Y');
+                $codSITUR = $fecha;
+            }
+
+            Comuna::create([
+                'nombre' => $nombre,
+                'cod_com' => $codCOM,
+                'cod_situr' => $codSITUR,
+                'cantidad_cc' => $cantidadCC,
+                'redis_id' => Redi::where('nombre', 'LLANOS')->first()?->id,
+                'estados_id' => Estado::where('nombre', 'GUARICO')->first()?->id,
+                'municipios_id' => Municipio::where('nombre_cne', $municipio)->first()?->id,
+                'parroquia' => $parroquia
+            ]);
+
+        }
+    }
+
+    public function startRow(): int
+    {
+        return 2;
+    }
+}
