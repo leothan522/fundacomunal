a6cafe63ed126a439b135736fb9aa064832d35d2
Yonathan Castillo <leothan522@gmail.com>
2025-12-11 07:27:36 -0400

    PreventBackHistory en Guest

---
+++ app/Http/Middleware/PreventBackHistory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PreventBackHistory
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        // Usando setCache para evitar que el navegador guarde la página
+        $response->setCache([
+            'no_store'        => true,
+            'no_cache'        => true,
+            'must_revalidate' => true,
+            'max_age'         => 0,
+        ]);
+
+        return $response;
+
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->web([
+           \App\Http\Middleware\PreventBackHistory::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
