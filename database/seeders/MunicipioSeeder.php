02022afa54c8aa67c2f96cab64d639d0fd2f93fb
Yonathan Castillo <leothan522@gmail.com>
2026-01-19 18:59:56 -0400

    Tipo de Comunas

--- database/seeders/MunicipioSeeder.php
+++ database/seeders/MunicipioSeeder.php
@@ -137,5 +137,49 @@ class MunicipioSeeder extends Seeder
             }
         }
 
+        $municipios = [
+            "CAMAGUAN" => "CAMAGUÁN",
+            "CHAGUARAMAS" => "CHAGUARAMAS",
+            "EL SOCORRO" => "EL SOCORRO",
+            "FRANCISCO DE MIRANDA" => "FRANCISCO DE MIRANDA",
+            "JOSE FELIX RIBAS" => "JOSÉ FÉLIX RIBAS",
+            "JOSE TADEO MONAGAS" => "JOSÉ TADEO MONAGAS",
+            "JUAN GERMAN ROSCIO" => "JUAN GERMÁN ROSCIO",
+            "JULIAN MELLADO" => "JULIÁN MELLADO",
+            "LAS MERCEDES" => "JUAN JOSE RONDON",
+            "LEONARDO INFANTE" => "LEONARDO INFANTE",
+            "ORTIZ" => "ORTIZ",
+            "PEDRO ZARAZA" => "PEDRO ZARAZA",
+            "SAN GERONIMO DE GUAYABAL" => "SAN GERÓNIMO DE GUAYABAL",
+            "SAN JOSE DE GUARIBE" => "SAN JOSÉ DE GUARIBE",
+            "SANTA MARIA DE IPIRE" => "SANTA MARÍA DE IPIRE",
+        ];
+
+        $municipiosCNE = [
+            "CAMAGUAN" => "MP. CAMAGUAN",
+            "CHAGUARAMAS" => "MP. CHAGUARAMAS",
+            "EL SOCORRO" => "MP. EL SOCORRO",
+            "FRANCISCO DE MIRANDA" => "MP. MIRANDA",
+            "JOSE FELIX RIBAS" => "MP. RIBAS",
+            "JOSE TADEO MONAGAS" => "MP. MONAGAS",
+            "JUAN GERMAN ROSCIO" => "MP. JUAN GERMAN ROSCIO N.",
+            "JULIAN MELLADO" => "MP. MELLADO",
+            "LAS MERCEDES" => "MP. JUAN JOSE RONDON",
+            "LEONARDO INFANTE" => "MP. INFANTE",
+            "ORTIZ" => "MP. ORTIZ",
+            "PEDRO ZARAZA" => "MP. ZARAZA",
+            "SAN GERONIMO DE GUAYABAL" => "MP.SAN GERONIMO DE G",
+            "SAN JOSE DE GUARIBE" => "MP.S JOSE DE GUARIBE",
+            "SANTA MARIA DE IPIRE" => "MP. S MARIA DE IPIRE",
+        ];
+
+        $data = \App\Models\Municipio::get();
+        foreach ($data as $municipio){
+            $nombre = $municipio->nombre;
+            $municipio->nombre_real = $municipios[$nombre] ?? $nombre;
+            $municipio->nombre_cne = $municipiosCNE[$nombre] ?? $nombre;
+            $municipio->save();
+        }
+
     }
 }
--- resources/views/exports/comunas.blade.php
+++ resources/views/exports/comunas.blade.php
@@ -7,6 +7,7 @@
         <th style="background-color: #C00000; color: #ffffff; border: 1px solid #404040; font-weight: bold; text-align: center">COd. SITUR</th>
         <th style="background-color: #C00000; color: #ffffff; border: 1px solid #404040; font-weight: bold; text-align: center">CIRCUITO O COMUNA</th>
         <th style="background-color: #C00000; color: #ffffff; border: 1px solid #404040; font-weight: bold; text-align: center">CANTIDAD C.C.</th>
+        <th style="background-color: #C00000; color: #ffffff; border: 1px solid #404040; font-weight: bold; text-align: center">TIPO</th>
     </tr>
     </thead>
     <tbody>
@@ -19,6 +20,20 @@
             <td style="border: 1px solid #404040; text-align: center">{{ \Illuminate\Support\Str::upper($data->cod_situr) }}</td>
             <td style="border: 1px solid #404040; text-align: center">{{ \Illuminate\Support\Str::upper($data->nombre) }}</td>
             <td style="border: 1px solid #404040; text-align: center">{{ $data->consejos->count() }}</td>
+            @php
+                $tipos = $data->consejos->map(fn($c) => $c->tipo->nombre)->unique();
+
+                $clasificacion = $tipos->count() === 1
+                    ? $tipos->first()
+                    : 'MIXTA';
+            @endphp
+
+            <td style="border: 1px solid #404040; text-align: center">
+                {{ \Illuminate\Support\Str::upper($clasificacion) }}
+            </td>
+
+
+
         </tr>
     @endforeach
 
